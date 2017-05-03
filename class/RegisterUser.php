7cfeedf0a64f88d6111331bf47ad034897ea61ac
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2017-05-03 13:44:09 -0300

    Including bootstrap grid responsive

--- class/RegisterUser.php
+++ class/RegisterUser.php
@@ -1,115 +1,122 @@
 <?php
+
 require_once 'UserBC.php';
+
 /**
  * Class responsible for manage data base
  * @date: 28-04-2017 
  * @author Marcelo
  */
-class RegisterUser extends UserBC{
-    private $hostDb;
-    private $userDb;
-    private $passDb;
-    private $dbnameDb;
-    private $validator;
-    private $connection;
-    private $msg;
-    
-    function __construct() {
-      parent::__construct();
-      $this->setHostDb('127.0.0.1');
-      $this->setUserDb('root');
-      $this->setPassDb(NULL);
-      $this->setDbnameDb('generate_passBC');
+class RegisterUser extends UserBC {
+
+  private $hostDb;
+  private $userDb;
+  private $passDb;
+  private $dbnameDb;
+  private $validator;
+  private $connection;
+  private $msg;
+
+  function __construct() {
+    parent::__construct();
+    $this->setHostDb('127.0.0.1');
+    $this->setUserDb('root');
+    $this->setPassDb(NULL);
+    $this->setDbnameDb('generate_passBC');
+    $this->setValidator(FALSE);
+
+    try {
+      $this->connection = new PDO("mysql:host={$this->getHostDb()}; dbname={$this->getDbnameDb()}", "root", NULL);
+    } catch (PDOException $ex) {
+      $ex->errorInfo($ex);
+    }
+  }
+
+  /**
+   * @name: validateFields
+   * @author Marcelo Pereira <marcelopereiraIFSP@gmail.com>
+   * @date: 03-05-2017;
+   * @description: validate fields is used for validate user register data, for not suportate sql injection.
+   * @return Call the function insertDB().
+   */
+  public function validateFields() {
+    if ($this->getRepass() === $this->getPass()):
+      $this->setMsg("Cadastro Realizado com Sucesso! :)");
+      $this->setValidator(TRUE);
+    else:
+      $this->setMsg("Opss! Houve um erro! Parece que as senhas digitadas não se coincidem!");
       $this->setValidator(FALSE);
-      
-      try{
-        $this->connection = new PDO("mysql:host={$this->getHostDb()}; dbname={$this->getDbnameDb()}", "root", NULL);
-      } catch (PDOException $ex) {
-        $ex->errorInfo($ex);
-      }
-    }
-    
-    /**
-     * @name: validateFields
-     * @author Marcelo Pereira <marcelopereiraIFSP@gmail.com>
-     * @date: 03-05-2017;
-     * @description: validate fields is used for validate user register data, for not suportate sql injection.
-     * @return Call the function insertDB().
-     */
-    public function validateFields(){
-      if ($this->getRepass() === $this->getPass()):
-        $this->setMsg("Cadastro Realizado com Sucesso! :)");
-        $this->setValidator(TRUE);
-      else:
-        $this->setMsg("Opss! Houve um erro! Parece que as senhas digitadas não se coincidem!");
-        $this->setValidator(FALSE);
-      endif;
-      
-      return $this->insertDB();
-    }
-    
-    /**
-     * @name: insertDB();
-     * @date: 03-05-2017;
-     * @description: Function used for to insert in data base the users datas resgisterds,
-     */
-    public function insertDB(){
-       if($this->getValidator()):
-         $this->connection->query("INSERT INTO `users` (name, phonePrimary, phoneSecundy, email, rg, cpf) VALUES ('{$this->getName()}', '{$this->getPhonePrimary()}', '{$this->getPhoneSecundy()}', '{$this->getEmail()}', '{$this->getRg()}', '{$this->getCpf()}');");
-       endif;
-    }
-    
-    public function getHostDb() {
-        return $this->hostDb;
-    }
+    endif;
 
-    public function getUserDb() {
-        return $this->userDb;
-    }
+    return $this->insertDB();
+  }
 
-    public function getPassDb() {
-        return $this->passDb;
-    }
+  /**
+   * @name: insertDB();
+   * @date: 03-05-2017;
+   * @description: Function used for to insert in data base the users datas resgisterds,
+   */
+  public function insertDB() {
+    if ($this->getValidator()):
+      $this->connection->query("INSERT INTO `users` (name, phonePrimary, phoneSecundy, email, rg, cpf) VALUES ('{$this->getName()}', '{$this->getPhonePrimary()}', '{$this->getPhoneSecundy()}', '{$this->getEmail()}', '{$this->getRg()}', '{$this->getCpf()}');");
 
-    public function getDbnameDb() {
-        return $this->dbnameDb;
-    }
+    endif;
+  }
 
-    public function setHostDb($hostDb) {
-        $this->hostDb = $hostDb;
-    }
+  public function getHostDb() {
+    return $this->hostDb;
+  }
 
-    public function setUserDb($userDb) {
-        $this->userDb = $userDb;
-    }
+  public function getUserDb() {
+    return $this->userDb;
+  }
 
-    public function setPassDb($passDb) {
-        $this->passDb = $passDb;
-    }
+  public function getPassDb() {
+    return $this->passDb;
+  }
 
-    public function setDbnameDb($dbnameDb) {
-        $this->dbnameDb = $dbnameDb;
-    }
-    function getValidator() {
-      return $this->validator;
-    }
+  public function getDbnameDb() {
+    return $this->dbnameDb;
+  }
 
-    function setValidator($validator) {
-      $this->validator = $validator;
-    }
-    function getConnection() {
-      return $this->connection;
-    }
+  public function setHostDb($hostDb) {
+    $this->hostDb = $hostDb;
+  }
 
-    function setConnection($connection) {
-      $this->connection = $connection;
-    }
-    
-    public function getMsg() {
-      return $this->msg;
-    }
+  public function setUserDb($userDb) {
+    $this->userDb = $userDb;
+  }
 
-    public function setMsg($msg) {
-      $this->msg = $msg;
-    }
+  public function setPassDb($passDb) {
+    $this->passDb = $passDb;
+  }
+
+  public function setDbnameDb($dbnameDb) {
+    $this->dbnameDb = $dbnameDb;
+  }
+
+  function getValidator() {
+    return $this->validator;
+  }
+
+  function setValidator($validator) {
+    $this->validator = $validator;
+  }
+
+  function getConnection() {
+    return $this->connection;
+  }
+
+  function setConnection($connection) {
+    $this->connection = $connection;
+  }
+
+  public function getMsg() {
+    return $this->msg;
+  }
+
+  public function setMsg($msg) {
+    $this->msg = $msg;
+  }
+
 }
--- index.php
+++ index.php
@@ -4,27 +4,33 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+
+        <!--STYLE SHEETS-->
+        <link href="css/plugins.css" type="text/css" rel="stylesheet"/>
+        <link href="css/style.css" type="text/css" rel="stylesheet"/>
     </head>
     <body>
-        <form action="index.php" method="post">
-            <span><?php echo $db->getMsg()?></span><br/>
-            <label>Nome:</label>
-            <input type="text" name="registerName"><br/>
-            <label>Celular:</label>
-            <input type="text" name="registerPhone1"><br/>
-            <label>Telefone:</label>
-            <input type="text" name="registerPhone2"><br />
-            <label>Email:</label>
-            <input type="text" name="registerEmail"><br />
-            <label>RG:</label>
-            <input type="text" name="registerRG"><br />
-            <label>CPF:</label>
-            <input type="text" name="registerCPF"><br />
-            <label>Senha:</label>
-            <input type="password" name="registerPass" /><br/>
-            <label>Repita a Senha:</label>
-            <input type="password" name="registerRepass" />
-            <button type="submit">Enviar</button>
-        </form>
+        <section class="container">
+            <form id="fm_register" action="index.php" method="post">
+                <span><?php echo $db->getMsg() ?></span><br/>
+                <label>Nome:</label>
+                <input type="text" name="registerName" required><br/>
+                <label>Celular:</label>
+                <input type="text" name="registerPhone1" required><br/>
+                <label>Telefone:</label>
+                <input type="text" name="registerPhone2" required><br />
+                <label>Email:</label>
+                <input type="text" name="registerEmail" required><br />
+                <label>RG:</label>
+                <input type="text" name="registerRG" required><br />
+                <label>CPF:</label>
+                <input type="text" name="registerCPF" required><br />
+                <label>Senha:</label>
+                <input type="password" name="registerPass" required/><br/>
+                <label>Repita a Senha:</label>
+                <input type="password" name="registerRepass" />
+                <button class="btn btn-primary" type="submit">Enviar</button>
+            </form>
+        </section>
     </body>
 </html>
